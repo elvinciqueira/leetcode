4b6ac70c4966c68bdb2a6acb8fd59de19aa3c4fe
elvinciqueira <elvinciqueira@gmail.com>
2023-01-13 14:47:49 -0300

    Running Sum

---
+++ 1480-Running-Sum-of-1d-Array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function runningSum(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] += nums[i - 1];
+  }
+  return nums;
+}
