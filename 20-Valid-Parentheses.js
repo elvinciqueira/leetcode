276a961ba0d366dac362870d2f45c92820d75420
elvinciqueira <elvinciqueira@gmail.com>
2023-01-24 12:20:30 -0300

    20. Valid Parentheses

---
+++ 20-Valid-Parentheses.js
@@ -0,0 +1,19 @@
+function isValid(s) {
+  const stack = [];
+  const map = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+  };
+  for (const char of s) {
+    const isBracket = char in map;
+    if (!isBracket) {
+      stack.push(char);
+    }
+    const isEqual = stack[stack.length - 1] === map[char];
+    if (isEqual) {
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
