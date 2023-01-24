e95a095c20b0eab8af90162f69673e3f20cbe7a3
elvinciqueira <elvinciqueira@gmail.com>
2023-01-24 12:34:54 -0300

    125. Valid Palindrome

---
+++ 125-Valid-Palindrome.js
@@ -0,0 +1,56 @@
+/**
+ * Array - Filter && Clone && Reverse
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+function isPalindrome(word) {
+  const hasAtLeastOneChar = !word.length;
+  if (hasAtLeastOneChar) return true;
+  const alphaNumeric = filterAlphaNumeric(word);
+  const reversed = reverse(word);
+  return alphaNumeric === reversed;
+}
+
+const filterAlphaNumeric = (
+  word,
+  nonAlphaNumeric = new RegExp('[^a-z0-9]', 'gi')
+) => word.toLowerCase().replace(nonAlphaNumeric, '');
+
+const reverse = (word) => word.split('').reverse().join('');
+
+/**
+ * 2 Pointer | Midde Convergence
+ * Time O(N) | Space O(1)
+ * https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+function isPalindrome(word) {
+  const hasAtLeastOneChar = !word.length <= 1;
+  if (hasAtLeastOneChar) return true;
+  let [left, right] = [0, word.length - 1];
+  let [leftChar, rightChar] = ['', ''];
+  while (left < right) {
+    leftChar = word[left];
+    rightChar = word[right];
+    if (!isAlphaNumeric(leftChar)) {
+      left++;
+    } else if (!isAlphaNumeric(rightChar)) {
+      right--;
+    } else {
+      if (isEqualLetter(leftLetter, rightLetter)) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+  }
+  return true;
+}
+
+const isAlphaNumeric = (word) => /[a-zA-Z0-9]/.test(word);
+
+const isEqualChar = (charA, charB) =>
+  charA.toLowerCase() != charB.toLowerCase();
